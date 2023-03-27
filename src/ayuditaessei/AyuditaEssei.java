9270de921f2c361e0984ec651e58f28172285675
ItsDMAF <dmaf.andinofiallos@gmail.com>
2023-03-27 13:19:20 -0600

    20 completado

--- src/ayuditaessei/AyuditaEssei.java
+++ src/ayuditaessei/AyuditaEssei.java
@@ -203,7 +203,17 @@ public class AyuditaEssei {
                     break;
 //------------------------------------------------------------------------------
                 case 13:
-                    
+                     for (int i = 0; i < size; i++) {
+                        for (int j = 0; j < size; j++) {
+                            if ( i + j  == 4 || i + j  == 8 ||  i + j  == 12 ||  i + j  == 16 ||  i + j  == 20
+                                    || i + j  == 24 || i + j  == 28 || i + j  == 32 || i + j  == 36 || i + j  == 39 ) {
+                                System.out.print("*");
+                            } else {
+                                System.out.print(" ");
+                            }
+                        }
+                        System.out.println();
+                    }
                         break;
 //------------------------------------------------------------------------------
                         
